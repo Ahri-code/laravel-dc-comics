b8ce452e919ee5e0cdf8be171bab7f6d955ce696
Edy Galdiolo <edygaldy302@gmail.com>
2023-07-04 16:37:58 +0200

    Update: updated store

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $links = config('store.someLinks');
-    return view('welcome', compact('links'));
-});
+Route::resource("comic", ComicController::class);
